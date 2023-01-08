f68dc947ebce134d4aca318eab2fb0cacb5d3573
nyllhad <nyllhad@hotmail.com>
2023-01-07 20:40:49 -0700

    Dahllyn Allsup Prep 2 - Conditions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int percentage = int.Parse(userInput);
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else if (percentage < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
     }
 }
