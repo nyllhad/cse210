af55f90addf1e67742bbfc7a3a46c1506fcadae7
nyllhad <nyllhad@hotmail.com>
2023-01-11 20:54:59 -0700

    Dahllyn Allsup Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbersList = new List<int>();
+        
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+            
+            if (userNumber != 0)
+            {
+                numbersList.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbersList)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbersList.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int max = numbersList[0];
+
+        foreach (int number in numbersList)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
